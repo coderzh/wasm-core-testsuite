8177258071ff6750a09f20cb302eafbd588f5d45
coderzh <coderzh@gmail.com>
2021-07-18 16:06:03 +0800

    add simd proposals script support

--- process_all.sh
+++ process_all.sh
@@ -11,7 +11,7 @@ export PATH=~/_Projects/Wasm/test/wabt/build:$PATH
 ./process.py --flags="--enable-tail-call"               ./_wast/proposals/tail-call ./proposals/tail-call
 ./process.py --flags="--enable-threads"                 ./_wast/proposals/threads ./proposals/threads
 ./process.py --flags="--enable-saturating-float-to-int" ./_wast/proposals/nontrapping-float-to-int-conversions ./proposals/nontrapping-float-to-int-conversions
+./process.py --flags="--enable-simd"                    ./_wast/proposals/simd ./proposals/simd
 
 # TODO:
-#./process.py --flags="--enable-simd"                   ./_wast/proposals/simd ./proposals/simd
 #./process.py --flags="--enable-exceptions"             ./_wast/proposals/___ ./proposals/___
